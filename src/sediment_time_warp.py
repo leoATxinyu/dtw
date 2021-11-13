48f3b99f8ea83b8f035af78be33125be1296ae7c
Andreas Paul <github@elcaptian.fastmail.com>
2021-11-13 23:01:33 +0400

    Code improvements to simple distance and preprocessing

--- src/sediment_time_warp.py
+++ src/sediment_time_warp.py
@@ -19,37 +19,39 @@ class SedimentTimeWarp:
 
         self.target: pd.DataFrame = target        
         self.data: pd.DataFrame = data
+        self.col_target_time: str = self.target.columns[0]
+        self.col_target_values: str = self.target.columns[1]
+        self.col_data_time: str = self.data.columns[0]
+        self.col_data_values: str = self.data.columns[1]
 
 
     @staticmethod
-    def smooth_time_series(time_series: Union[pd.core.series.Series, list], window_size: int = 11, polynomial: int = 3):
+    def smooth_time_series(time_series: Union[pd.core.series.Series, list, np.array], 
+                            window_size: int = 11, polynomial: int = 3):
         """Smooth a time-series using Savitzky-Golay smoothing algorithm
         """        
         if isinstance(time_series, (pd.core.series.Series, np.array, list)):
             return savitzky_golay(time_series, window_size, polynomial)
         else:
-            raise TypeError('Time series needs to be of type pd.Series, np.array or list')
+            raise TypeError('Time series needs to be of type pandas.Series, numpy.array or list')
 
 
     def simple_distance(self, target_col_name: str, data_col_name: str, 
                         target_max_age: int = None, use_smoothed: bool = False, 
                         window_size: int = None, polynomial: int = None):
-        """Calculate distance for a given target/data pair
+        """Calculate Euclidian distance for a given target/data pair
         """
-
         if use_smoothed:
             if not window_size or not polynomial:
                 print("ERROR: Missing 'window_size' or 'polynomial' parameter.")
                 return
             self.data[data_col_name] = self.smooth_time_series(self.data[data_col_name], window_size, polynomial)
-
         if target_max_age:
-            target = target['']
-        print(self.data[data_col_name])
-
-        print(self.target[target_col_name])
-        
-        self.distance: float = dtw.distance(self.data[data_col_name], self.target[target_col_name])
+            target = self.target[self.col_target_time] <= target_max_age
+        else:
+            target = self.target[self.col_target_values]
+        data = self.data[data_col_name]
+        self.distance: float = dtw.distance(data, target)
         print(f'Distance: {self.distance}')
 
 
@@ -93,8 +95,8 @@ class SedimentTimeWarp:
 
     def monte_carlo(self):
         """Perform complex Monte Carlo simulation of various 
-        parameters to find minimum distance(s) for a given 
-        target/data pair.        
+        parameters to find minimum Euclidian distance(s) for 
+        given target/data pair.        
         """
         pass
 
@@ -110,8 +112,9 @@ if __name__ == "__main__":
 
     df_1100 = pd.read_csv('data/core_1100.csv')
     stack = pd.read_csv('data/LR04stack.txt', sep='\\t', engine='python')       
-    test_dtw = SedimentTimeWarp(stack, df_1100)
-    test_dtw.simple_distance(target_col_name='Benthic_d18O_per-mil', data_col_name='d18O_pl', use_smoothed=True, window_size=11, polynomial=3)
+    test_dtw = SedimentTimeWarp(target = stack, data = df_1100)
+    test_dtw.simple_distance(target_col_name='Benthic_d18O_per-mil', data_col_name='d18O_pl', target_max_age=350)
+    
     # distances = test_dtw.minimize_distance(start_time=100, end_time=1000, step_size=100)
     # path = test_dtw.get_warping_path()
     # print(distances.values())
